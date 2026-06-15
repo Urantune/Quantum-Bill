7d96df8cd209019cd75231f7a403431bf62f4882
Trong Tri <tritrongdo111@gmail.com>
2026-06-15 08:16:04 +0700

    commit1

---
+++ Back/src/test/java/quantum_bill/stock/StockApplicationTests.java
@@ -0,0 +1,13 @@
+package quantum_bill.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
